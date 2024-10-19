4d56ee7ffbdfaa0f8cdfe4f4cf09dcf1be1bc3aa
teacher <tibedox@mail.ru>
2024-10-19 16:57:57 +0600

    Base version

--- app/src/main/java/com/example/satbober/MainActivity.java
+++ app/src/main/java/com/example/satbober/MainActivity.java
@@ -12,9 +12,7 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
     int counter = 0;
-    String sDown, sUp;
     TextView textCounter;
-    TextView textHeader;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,29 +24,11 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        textHeader = findViewById(R.id.text1);
         textCounter = findViewById(R.id.text2);
     }
 
     public void tapBober(View view) {
         counter += 1;
-        sDown = "Потрогали " + counter + ok(counter);
-        sUp = "Потрогай бобра!";
-        if(counter == 10) {
-            sUp = "Трогай, трогай, не останавливайся!";
-        }
-        if(counter > 20 & counter < 25) {
-            sUp = "Ты молодец! трогай дальше!";
-        }
-        textHeader.setText(sUp);
-        textCounter.setText(sDown);
-    }
-
-    String ok(int x) {
-        String str = " раз";
-        if(x%10>1 & x%10<5 & x/10%10!=1){
-            str = " раза";
-        }
-        return str;
+        textCounter.setText("Потрогали " + counter + " раз");
     }
 }
